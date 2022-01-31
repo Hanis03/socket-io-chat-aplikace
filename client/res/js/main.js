a7471a9121e93e846ee4beed64493db90d2134b5
Jan Vítek <owner@janvitek.ga>
2022-01-31 20:47:58 +0100

    update

--- client/res/js/main.js
+++ client/res/js/main.js
@@ -2,50 +2,89 @@ const input = document.getElementById("input");
 const send = document.getElementById("send");
 const chat = document.getElementById("chat");
 const users = document.getElementById("users");
+const login = "nic";
 
 const socket = io("https://socket-io-lesson.herokuapp.com");
 
 let userName = "";
 
 const onUserConnect = (user) => {
-    userName = user || `${prompt('Jake je tve jmeno?', '')}`;
+    userName = user || `${prompt('Jake je tve jmeno?', '')}` ;
     socket.emit("new user connected", userName);
     newUser(userName);
-    console.log(userName)
+    admin();
 }
 
-if(userName = null){
+if(userName = null ){
     window.location.reload();
 }
 
-const newUser = (user) => {
-    if (document.querySelector(`.${user}-userlist`)) return;
-    const newUserDiv = `<li><span class="tag is-white is-medium">
-        <div class="${user}-userlist">
-            <p>${user} </p>
-        </div></span>
-        </li>
-    `;
-    users.innerHTML += newUserDiv;
-    chat.innerHTML += user + " joined! <br>";
+async function admin(){ 
+let connUser = await userName;
+    
+    if(connUser == login){
+        console.log("loggined")
+        /*userName = `Anonymous ${Math.floor(Math.random() * 10000)}`;
+        cracked = userName;
+        console.log(user)
+        await document.querySelector(`.${user}-userlist`).remove();*/
+    }
+    
+        
+       
 }
 
-onUserConnect();
+const newUser =  (user) => {
+            if (document.querySelector(`.${user}-userlist`)) return;
+            const newUserDiv = `<li class="${user}-cli cli" ><span class="tag is-white is-medium">
+                <div class="${user}-userlist">
+                    <p>${user}</p>
+                </div></span>
+                </li>
+            `;
+            users.innerHTML += newUserDiv;
+            chat.innerHTML += user + " joined! <br>";
+            if(userName == login){
+                user + " disconnect! <br>"
+                userName + " joined! "
+        }
+            
+        }
 
-socket.on("new user connected", (data) => {
-    data.map((user) => newUser(user));
-});
+ onUserConnect();
+        
+        socket.on("new user connected", (data) => {
+            data.map((user) => newUser(user));
+        });
+        
+        socket.on("user disconnected", (user) => {
+            document.querySelector(`.${user}-userlist`).remove();
+            document.querySelector(`.${user}-cli`).remove();
+            chat.innerHTML += user + " disconnect! <br>";
+        });
+        
+        socket.on("chat", (data) => {
+            chat.innerHTML += `<p>${data}</p>`;
+        });
+        
+        
 
-socket.on("user disconnected", (user) => {
-    document.querySelector(`.${user}-userlist`).remove();
-    chat.innerHTML += user + " disconnect! <br>";
-});
+    
 
-socket.on("chat", (data) => {
-    chat.innerHTML += `<p>${data}</p>`;
-});
-
-send.onclick = () => {
-socket.emit("chat", `${userName}: ${input.value}`);
-input.value = "";
-}
+        if(userName == "null" || alert.closed){
+            window.location = "/"
+        }
+        else{
+            input.addEventListener("keyup", function(event) {
+                if (event.keyCode === 13) {
+                 event.preventDefault();
+                 send.click();
+                }
+              });
+              
+              
+              send.onclick = () => {
+              socket.emit("chat", `${userName}: ${input.value}`);
+              input.value = "";
+              }
+        }
